e1121ae0e0eb5c69aaefea20d7a8d56fabf89dde
Joonyoung Yi <joonyoung@bablabs.com>
2018-06-20 15:32:30 +0900

    database real init

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, Boolean
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -9,22 +9,38 @@ engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 Base = declarative_base()
 
 
-class User(Base):
-    __tablename__ = 'users'
+class Item(Base):
+    __tablename__ = 'item'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    fullname = Column(String(50))
-    password = Column(String(50))
+    id = Column(Integer, nullable=False, primary_key=True)
+    key = Column(String(10), index=True, default='')
 
-    def __init__(self, name, fullname, password):
-        self.name = name
-        self.fullname = fullname
-        self.password = password
+    name = Column(String(150), index=True, default='')
+    year = Column(Integer, index=True)
 
-    def __repr__(self):
-        return "<User('%s', '%s', '%s')>" % (self.name, self.fullname,
-                                             self.password)
+    release_date = Column(Date, index=True)
+    video_release_date = Column(Date)
+
+    old_imdb_url = Column(String(1000), default='')
+
+    category_action = Column(Boolean, default=False, nullable=False)
+    category_adventure = Column(Boolean, default=False, nullable=False)
+    category_animation = Column(Boolean, default=False, nullable=False)
+    category_children = Column(Boolean, default=False, nullable=False)
+    category_comedy = Column(Boolean, default=False, nullable=False)
+    category_crime = Column(Boolean, default=False, nullable=False)
+    category_documentary = Column(Boolean, default=False, nullable=False)
+    category_drama = Column(Boolean, default=False, nullable=False)
+    category_fantasy = Column(Boolean, default=False, nullable=False)
+    category_flim_noir = Column(Boolean, default=False, nullable=False)
+    category_horror = Column(Boolean, default=False, nullable=False)
+    category_musical = Column(Boolean, default=False, nullable=False)
+    category_mystery = Column(Boolean, default=False, nullable=False)
+    category_romance = Column(Boolean, default=False, nullable=False)
+    category_sci_fi = Column(Boolean, default=False, nullable=False)
+    category_thriller = Column(Boolean, default=False, nullable=False)
+    category_war = Column(Boolean, default=False, nullable=False)
+    category_western = Column(Boolean, default=False, nullable=False)
 
 
 # print(User.__table__)
