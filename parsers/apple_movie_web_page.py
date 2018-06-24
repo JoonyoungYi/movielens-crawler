6d1f2eb01ee3ee609d8e75631b38e98cb3f91bf0
Joonyoung Yi <joonyoung@bablabs.com>
2018-06-24 17:43:03 +0900

    apple movie web page parse

---
+++ parsers/apple_movie_web_page.py
@@ -0,0 +1,67 @@
+from traceback import format_exc
+
+import requests
+
+from models import Session
+from models import AppleMovie
+from models import WebPage
+
+
+def __request(path):
+    try:
+        r = requests.get('https://itunes.apple.com/us/movie{}'.format(path))
+        return r.text
+    except:
+        print(format_exc())
+        return None
+
+
+def __save(key, text):
+    with open('cdn/{}.html'.format(key), 'w') as f:
+        f.write(text)
+        return True
+    return False
+
+
+def _request_and_save(key, path):
+    text = __request(path)
+    if not text:
+        return False
+
+    return __save(key, text)
+
+
+def main():
+    session = Session()
+
+    for idx, apple_movie in enumerate(
+            session.query(AppleMovie).filter(AppleMovie.web_page_id.is_(
+                None))):
+        # if apple_movie.id < 1623:
+        #     continue
+
+        print(
+            '>>',
+            '{:4d}'.format(apple_movie.id), )
+        if not apple_movie.url:
+            continue
+
+        web_page = WebPage()
+        session.add(web_page)
+        apple_movie.web_page = web_page
+        session.commit()
+
+        success = _request_and_save(web_page.key, apple_movie.url)
+        if not success:
+            session.delete(web_page)
+            apple_movie.web_page = None
+            session.commit()
+            print('  [-] Fail')
+        else:
+            print('  [+] Success')
+
+        # break
+
+
+if __name__ == '__main__':
+    main()
